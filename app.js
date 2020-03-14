7ece043e9d997ef7f32d344f5b07284c250fbdfc
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 15:38:57 +0100

    Added (index.html, contact.html and 404.hmtl) and made routes

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
 	console.log("request was me: " + req.url);
-	res.write(200, { "Content-Type": "text/plain" });
-	res.end("feed me popcorn");
+	if (req.url === "/home" || req.url == "/") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		fs.createReadStream(__dirname + "/index.html").pipe(res);
+	} else if (req.url === "/contact") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		fs.createReadStream(__dirname + "/contact.html").pipe(res);
+	} else if (req.url === "/api") {
+		const ninjas = [
+			{ name: "ryu", age: 29 },
+			{ name: "Nicolaj", age: 62 }
+		];
+
+		res.writeHead(200, { "Content-Type": "application/json" });
+		res.end(JSON.stringify(ninjas));
+	} else {
+		res.writeHead(404, { "Content-Type": "text/html" });
+		fs.createReadStream(__dirname + "/404.html").pipe(res);
+	}
 });
 
 server.listen(5050, "127.0.0.1");
